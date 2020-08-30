439fa3c4d1d24f764ed9ffd0ca9034050385772f
Volodya <volodyavolga@protonmail.ch>
2020-08-31 01:34:00 +0300

    not bad

---
+++ MyApp/Models/UserInfo.swift
@@ -0,0 +1,14 @@
+//
+//  UserInfo.swift
+//  MyApp
+//
+//  Created by Vladimir Stepanchikov on 29.08.2020.
+//  Copyright © 2020 Vladimir Stepanchikov. All rights reserved.
+//
+
+import UIKit
+
+struct UserInfo {
+    let login: String
+    let password: String
+}
---
+++ MyApp/ViewControllers/WelcomeVC.swift
@@ -0,0 +1,13 @@
+//
+//  WelcomeVC.swift
+//  MyApp
+//
+//  Created by Vladimir Stepanchikov on 29.08.2020.
+//  Copyright © 2020 Vladimir Stepanchikov. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+    
+}
